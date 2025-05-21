c69b7129023ab4930aae0d552c7487d4fc0bc8c7
adrian <profilte82@gmail.com>
2025-05-21 10:19:05 +0100

    Cambios en contacto

--- contacto.php
+++ contacto.php
@@ -3,9 +3,9 @@
 <div id="principal">
     <h1>Adrián Santana Suárez</h1>
     <p>Puedes Contactar conmigo en: adriansantanadev@gmail.com</p>
-
+    <br>
     <a href="https://www.linkedin.com/in/adri%C3%A1n-santana-su%C3%A1rez/">Linkedin</a>
-
+    <br>
     <a href="https://github.com/Adriancito82">GitHub</a>
 </div>
 
