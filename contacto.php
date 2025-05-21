8eb1a06333be55fd5eca7b35cf13f972f66b4f1f
adrian <profilte82@gmail.com>
2025-05-21 10:15:31 +0100

    Cambios en contacto

--- contacto.php
+++ contacto.php
@@ -2,9 +2,11 @@
 
 <div id="principal">
     <h1>Adrián Santana Suárez</h1>
-    <p>Puedes Contactar conmigo en: </p>
-        <a href="adriansantanadev@gmail.com">adriansantanadev@gmail.com</a>
-        <p>Linkedin: <a href="https://www.linkedin.com/in/adri%C3%A1n-santana-su%C3%A1rez/"></a>Linkedin Adrián</p>
+    <p>Puedes Contactar conmigo en: adriansantanadev@gmail.com</p>
+
+    <a href="https://www.linkedin.com/in/adri%C3%A1n-santana-su%C3%A1rez/">Linkedin</a>
+
+    <a href="https://github.com/Adriancito82">GitHub</a>
 </div>
 
  <?php require_once 'includes/pie.php'; ?>
